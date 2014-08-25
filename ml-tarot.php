d5864243c2bfe641112cb7f43c1548dfb34e6273
griansun <griansun@hotmail.com>
2014-08-25 21:29:15 +0200

    Display tarotspread names

--- ml-tarot.php
+++ ml-tarot.php
@@ -18,8 +18,23 @@ function ml_tarot_spread_overview_handler() {
 
 function ml_tarot_spread_overview_function() {
   //process plugin
-  $demolp_output = "Hello World!";
+  $demolp_output = "Leggingen: ";
   //send back text to calling function
-  return $demolp_output;
+  global $wpdb;
+  $result = $wpdb->get_results(
+	"
+	SELECT * FROM tarotspread
+	"
+    );
+
+    $demolp_output = '<ul>';
+    foreach( $result as $results ) {
+
+        $demolp_output = $demolp_output . '<li>' . $results->name .'</li>';
+    }
+    $demolp_output = $demolp_output . '</ul>';
+    return $demolp_output;
 }
+
+
 ?>
